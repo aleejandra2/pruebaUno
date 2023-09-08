5c62b0752913d43104a8e604af0c6435791f3876
ZMarv <anto.rojas@duocuc.cl>
2023-09-08 00:47:22 -0300

    Cambios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
+import { Usuario } from '../registrar/registrar.page';;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  formLogin!: FormGroup;
+  usuarios: Usuario[] = [];
+
+  constructor(public formBuilder: FormBuilder,private alertController: AlertController) {
+
+    this.formLogin = this.formBuilder.group({
+      'correo': new FormControl("",Validators.required),
+      'contraseña': new FormControl("",Validators.required)
+    })
+  }
+
+  async login(){
+    var form = this.formLogin.value;
+    
+    const usuarioString = localStorage.getItem('usuarios');
+
+  if (usuarioString) {
+    this.usuarios = JSON.parse(usuarioString);
+
+    const usuarioEncontrado = this.usuarios.find(usuario => usuario.correo === form.correo && usuario.contraseña === form.contraseña);
+
+    if (usuarioEncontrado) {
+      console.log('ingresar')
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Datos Incorrectos',
+        message: 'Tiene que completar todos los datos',
+        buttons: ['Aceptar'],
+      });
+      await alert.present();
+    }
+  } else {
+    const alert = await this.alertController.create({
+      header: 'Usuario no encontrado',
+      message: 'El usuario no existe',
+      buttons: ['Aceptar'],
+    });
+    await alert.present();
+  }
+}
 
 }
--- src/app/registrar/registrar.page.ts
+++ src/app/registrar/registrar.page.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
+
+
+export interface Usuario {
+  nombre: string;
+  apellido: string;
+  rut: string;
+  carrera: string;
+  correo: string;
+  contraseña: string;
+}
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.page.html',
@@ -7,9 +19,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  formRegistro!: FormGroup;
+  usuarios: Usuario[] = [];
+
+  constructor(public formBuilder: FormBuilder,private alertController: AlertController) {
+    this.formRegistro = this.formBuilder.group({
+      'nombre': new FormControl("",Validators.required),
+      'apellido': new FormControl("",Validators.required),
+      'rut': new FormControl("",Validators.required),
+      'carrera': new FormControl("",Validators.required),
+      'correo': new FormControl("",Validators.required),
+      'contraseña': new FormControl("",Validators.required)
+    });
+   }
 
   ngOnInit() {
+    const usuariosRegistrados = localStorage.getItem('usuarios');
+    if (usuariosRegistrados) {
+      this.usuarios = JSON.parse(usuariosRegistrados);
+    }
   }
 
+
+  async registrar(){
+    console.log("Guardar")
+    var form = this.formRegistro.value;
+
+    if(this.formRegistro.invalid){
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        message: 'Tiene que completar todos los datos',
+        buttons: ['Aceptar'],
+      });
+  
+      await alert.present();
+      return;
+    }
+    var nuevoUsuario: Usuario = {
+      nombre: form.nombre,
+      apellido: form.apellido,
+      rut: form.rut,
+      carrera: form.carrera,
+      correo: form.correo,
+      contraseña: form.contraseña,
+    };
+    this.usuarios.push(nuevoUsuario);
+
+    localStorage.setItem('usuarios', JSON.stringify(this.usuarios));
+    
+   
+  }
 }
